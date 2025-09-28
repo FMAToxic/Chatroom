2003eaa454fd39de0731cfce022dab9d89190267
FMA Toxic <mintyoculus@gmail.com>
2025-09-28 17:01:36 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
+// server.js
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('chat message', (data) => {
+    io.emit('chat message', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+const PORT = 3000;
+http.listen(PORT, () => {
+  console.log(`Chatroom running at http://localhost:${PORT}`);
+});
 
